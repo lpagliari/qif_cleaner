1bcaa85777b47f9272da7fc2074e6314a7c1ec98
Luiza Pagliari <lpagliar@thoughtworks.com>
2013-07-11 20:59:04 -0300

    Adding simple file with the pt_BR MS Money 97 format

--- main.rb
+++ main.rb
@@ -1,9 +1,9 @@
 require 'qif'
 require File.dirname(__FILE__) + '/lib/base'
 
-data = open('files/3_records_mmddyyyy.qif').read
+data = open('files/br_clear.qif').read
 
-qif = Qif::Reader.new(data)
+qif = Qif::Reader.new(data, "mm/dd'yy")
 puts "size = #{qif.size}"
 qif.each do |transaction|
   puts [transaction.date, transaction.memo, transaction.amount].join(', ')
