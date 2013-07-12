c0667bf2630eda6e919119df3771a889b4524bbe
Luiza Pagliari <lpagliar@thoughtworks.com>
2013-07-11 23:10:36 -0300

    Cleaning up special chars when reading file

--- main.rb
+++ main.rb
@@ -1,10 +1,12 @@
 require 'qif'
 require File.dirname(__FILE__) + '/lib/base'
 
-data = open('files/br_clear.qif').read
+data = open('files/br_dirty.qif').read
+clean_data = SpecialCharRemover.clear(data)
+puts clean_data
 
-qif = Qif::Reader.new(data, "mm/dd'yy")
-puts "size = #{qif.size}"
-qif.each do |transaction|
-  puts [transaction.date, transaction.memo, transaction.amount].join(', ')
-end
+#qif = Qif::Reader.new(data, "m/d'yy")
+#puts "size = #{qif.size}"
+#qif.each do |transaction|
+#  puts [transaction.date, transaction.memo, transaction.amount].join(', ')
+#end
