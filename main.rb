9da57576e2e036d453c9e6da46a42363e767b2bf
Luiza Pagliari <lpagliar@thoughtworks.com>
2013-07-14 09:38:36 -0300

    Exporting to a new QIF file

--- main.rb
+++ main.rb
@@ -1,10 +1,22 @@
 require 'qif'
 require File.dirname(__FILE__) + '/lib/base'
 
-data = open('files/ddmmyy_dirty.qif').read
+files = ['ddmmyy_dirty.qif']
+files.each do |file| 
+  data = open("files/#{file}").read
 
-qif = BrQif.new(data).read
-puts "size = #{qif.size}"
-qif.each do |transaction|
-  puts [transaction.date, transaction.memo, transaction.amount, transaction.category, transaction.payee].join(', ')
+  qif = BrQif.new(data).read
+
+  original_transactions = data.count("^")
+  puts "Transactions read for file '#{file}': #{qif.size} (out of #{original_transactions})"
+
+  #qif.each do |transaction|
+  #  puts [transaction.date, transaction.memo, transaction.amount, transaction.category, transaction.payee].join(', ')
+  #end
+
+  Qif::Writer.open("files/exported/clear_#{file}") do |writer|
+    qif.transactions.each do |transaction|
+      writer << transaction
+    end
+  end
 end
