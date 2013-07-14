421388a04434d8b7744088743a813b7411e67364
Luiza Pagliari <lpagliar@thoughtworks.com>
2013-07-14 18:48:29 -0300

    Some more adjustments to open an ISO-8859 file

--- main.rb
+++ main.rb
@@ -4,12 +4,12 @@ require File.dirname(__FILE__) + '/lib/base'
 ARGV.each do |full_path_to_file|
   file = File.basename(full_path_to_file)
 
-  data = open("files/#{file}").read
+  data = open(full_path_to_file, "r:iso-8859-1:utf-8").read
 
   qif = BrQif.new(data).read
 
   original_transactions = data.count("^")
-  puts "Transactions read for file '#{file}': #{qif.size} (out of #{original_transactions})"
+  puts "Transactions read for file '#{full_path_to_file}': #{qif.size} (out of #{original_transactions})"
 
   #qif.each do |transaction|
   #  puts [transaction.date, transaction.memo, transaction.amount, transaction.category, transaction.payee].join(', ')
